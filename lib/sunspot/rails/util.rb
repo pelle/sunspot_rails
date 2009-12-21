d7632643b31ead4e6aa0d84a7c7b332aef8a0c3d
Pelle Braendgaard <pelleb@gmail.com>
2009-12-21 13:02:19 -0500

    Patched it with the patch from humanzz at: http://outoftime.lighthouseapp.com/projects/20339/tickets/46-an-issue-with-sunspot-rails-and-single-table-inheritance

--- lib/sunspot/rails/util.rb
+++ lib/sunspot/rails/util.rb
@@ -8,9 +8,19 @@ module Sunspot #:nodoc:
           @sunspot_options ||= {}
         end
         
-        def index_relevant_attribute_changed?( object )
-          class_key = object.class.to_s.underscore.to_sym
-          ignore_attributes = (sunspot_options[class_key][:ignore_attribute_changes_of] || [])
+        def index_relevant_attribute_changed?(object)
+          klass = object.class
+          class_keys = []
+          while klass != ActiveRecord::Base do
+            class_keys << klass.to_s.underscore.to_sym
+            klass = klass.superclass
+          end   
+          class_sunspot_options = nil
+          class_keys.each do |key|
+            class_sunspot_options = sunspot_options[key]
+            break unless class_sunspot_options.nil?
+          end
+          ignore_attributes = (class_sunspot_options[:ignore_attribute_changes_of] || [])
           !(object.changes.symbolize_keys.keys - ignore_attributes).blank?
         end
         
